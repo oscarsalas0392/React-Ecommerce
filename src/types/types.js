6c44489b4ff0900acc536ccf0dc1beed0cb21db6
Oscar Salas Miranda <73209356+oscarsalas0392@users.noreply.github.com>
2021-12-08 05:40:37 -0600

    bug types

--- src/types/types.js
+++ src/types/types.js
@@ -5,6 +5,6 @@ export const types ={
      categoriaGet:'[categoria] Buscar categorias',
      categoriaGetId:'[categoria] Buscar categoria',
      categoriaAdd:'[categoria] Agregar categoria',
-     categoriaDelete:'[categoria] Eliminar categoria'
+     categoriaDelete:'[categoria] Eliminar categoria',
      categoriaUpdate:'[categoria] Actualizar categoria'
 }
